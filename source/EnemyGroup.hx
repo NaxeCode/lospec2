a466b8193d0c6bb27c0e5febbac364989eb341b4
Aladdin Ali <15963333+NaxeCode@users.noreply.github.com>
2024-05-06 14:27:41 -0400

    added enemy spawner

--- source/EnemyGroup.hx
+++ source/EnemyGroup.hx
@@ -3,16 +3,19 @@ package;
 import flixel.FlxG;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.util.FlxSpriteUtil;
+import flixel.util.FlxTimer;
 
 class EnemyGroup extends FlxTypedGroup<Enemy>
 {
 	static inline var enemyCount:Int = 10;
+	var spawnTimer:FlxTimer;
 
 	public function new()
 	{
 		super(enemyCount);
 
 		addEnemies();
+		setTimer();
 	}
 
 	function addEnemies()
@@ -20,10 +23,21 @@ class EnemyGroup extends FlxTypedGroup<Enemy>
 		for (i in 0...enemyCount)
 		{
 			var enemy = new Enemy(0, 0);
+			enemy.kill();
 			add(enemy);
 		}
 
-		FlxSpriteUtil.space(cast this.members, FlxG.width / 10, FlxG.height / 10, 35, null, false);
+		// FlxSpriteUtil.space(cast this.members, FlxG.width / 10, FlxG.height / 10, 35, null, false);
+	}
+
+	function setTimer()
+	{
+		spawnTimer = new FlxTimer().start(1, (timer:FlxTimer) ->
+		{
+			var enemy = recycle();
+			enemy.setPosition(FlxG.width, FlxG.random.int(0, FlxG.height - 16));
+			enemy.velocity.x = -100;
+		}, 0);
 	}
 
 	override function update(elapsed:Float)
