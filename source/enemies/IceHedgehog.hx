57a87f8c0ccebf9b48c7c354c5991c2da68587be
Aladdin Ali <15963333+NaxeCode@users.noreply.github.com>
2024-05-16 13:40:51 -0400

    Made ice hedgehog bounce around instead of screenwrapping

--- source/enemies/IceHedgehog.hx
+++ source/enemies/IceHedgehog.hx
@@ -1,5 +1,7 @@
 package enemies;
 
+import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.util.FlxColor;
 import flixel.util.FlxSpriteUtil;
 
@@ -23,6 +25,31 @@ class IceHedgehog extends Enemy
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
-		FlxSpriteUtil.screenWrap(this);
+		bounce(this);
+	}
+
+	public function bounce(sprite:FlxSprite):FlxSprite
+	{
+		// Left
+		if (((sprite.x + sprite.frameWidth / 2) <= 0))
+		{
+			sprite.velocity.x = -sprite.velocity.x;
+		}
+		// Right
+		else if ((sprite.x >= FlxG.width))
+		{
+			sprite.velocity.x = -sprite.velocity.x;
+		}
+		// UP
+		if (((sprite.y + sprite.frameHeight / 2) <= 0))
+		{
+			sprite.velocity.y = -sprite.velocity.y;
+		}
+		// Bottom
+		else if ((sprite.y >= FlxG.height))
+		{
+			sprite.velocity.y = -sprite.velocity.y;
+		}
+		return sprite;
 	}
 }
